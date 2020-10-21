4f9656074b3fb10b17158c1f4ce2fe63ae1c44b7
Krivv9 <kamil.racki@gmail.com>
2020-10-21 23:13:49 +0200

    create services

---
+++ src/main/java/pl/coderslab/charity/services/CategoryService.java
@@ -0,0 +1,16 @@
+package pl.coderslab.charity.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.repositories.CategoryRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryService {
+}
---
+++ src/main/java/pl/coderslab/charity/services/InstitutionService.java
@@ -0,0 +1,14 @@
+package pl.coderslab.charity.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class InstitutionService {
+}
